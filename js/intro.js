506f6888ad86c284ba3d147d74ddc35157b8bb58
knking <knr020498@gmail.com>
2022-12-12 19:13:31 +0530

    new html css project

--- js/intro.js
+++ js/intro.js
@@ -1,8 +1,16 @@
 let a =10;
 let b=20;
 let c =a + b;
-console.log(c)
+// console.log(c)
 
-console.log(Math.round(9.6))
-console.log(Math.ceil(9.9))
-console.log(Math.floor(9.9))
+// console.log(Math.round(.6))
+// console.log(Math.ceil(9.9))
+// console.log(Math.floor(9.9))
+console.log(Math.round(Math.random() * 50))
+
+console.log(Math.pow(3,3))
+console.log(Math.sin(90))
+
+                      // loops
+    
+                      
