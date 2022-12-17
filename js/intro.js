d5eff5d58c45785981701d1a4fb10ea73e933b29
knking <knr020498@gmail.com>
2022-12-17 23:01:08 +0530

    js

--- js/intro.js
+++ js/intro.js
@@ -1,16 +1,6 @@
-let a =10;
-let b=20;
-let c =a + b;
-// console.log(c)
+let myArr = ["krishna", "nand", "ray", 1000, 9.0, true];
 
-// console.log(Math.round(.6))
-// console.log(Math.ceil(9.9))
-// console.log(Math.floor(9.9))
-console.log(Math.round(Math.random() * 50))
-
-console.log(Math.pow(3,3))
-console.log(Math.sin(90))
-
-                      // loops
-    
-                      
+console.log(myArr[0]);
+console.log(myArr[1]);
+console.log(myArr[2]);
+console.log(myArr[3]);
