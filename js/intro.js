c7fd79b917ed9aa30ac439caa0fcc044d872c2f3
knking <knr020498@gmail.com>
2022-12-17 23:47:01 +0530

    js

--- js/intro.js
+++ js/intro.js
@@ -1,7 +1,6 @@
 // let myArr = ["krishna","nand","ray",1000,9.0,true];
 // console.log(myArr.length);
-let array1 = ['krishna', 'ray', 'nand'];
-let array2 = [1, 2, 3];
-
-let array3 = array1.concat(array2);
-console.log(array3)
+let arr1 = ['krishna', 'ray', 'nand'];
+let arr2 = [1, 2, 3];
+let arr3 = arr1.concat(arr2);
+console.log(arr3)
