8a9402d53bc65205e767875c42512d2dd3c45c68
knking <knr020498@gmail.com>
2022-12-18 10:35:42 +0530

    pop

--- js/intro.js
+++ js/intro.js
@@ -24,11 +24,20 @@
 
              // push
 
- let list_of_eleemnt = [10,50,90,100];
+//  let list_of_eleemnt = [10,50,90,100];
+//  console.log("original array:- ", list_of_eleemnt)
+//  list_of_eleemnt.push(60)
+//  list_of_eleemnt.push("krishna")
+//  list_of_eleemnt.push('a')
+//  list_of_eleemnt.push(10.10)
+//  console.log("After push operation new array is :- ")
+//  console.log(list_of_eleemnt)
 
- list_of_eleemnt.push(60)
- list_of_eleemnt.push("krishna")
- list_of_eleemnt.push('a')
- list_of_eleemnt.push(10.10)
+               // pop
 
- console.log(list_of_eleemnt)
+               let arr = [ 10, 50, 90, 100, 60, 'krishna', 'a', 10.1 ];
+               arr.pop();
+               console.log(arr)
+               
+               let poped_element = arr.pop()
+               console.log(poped_element);
