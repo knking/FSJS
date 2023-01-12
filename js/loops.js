5c859ade4f66108a24a8c9f9e91dc2e5e2a2c5d0
knking <knr020498@gmail.com>
2023-01-12 22:24:00 +0530

    js loop

---
+++ js/loops.js
@@ -0,0 +1,14 @@
+// for loop
+
+// for(let i=1;i<=10;i++){
+//     console.log("2 * ", i, "=",i*2);
+// }
+
+// while loop
+
+let i=0;
+while(i<20){
+
+   console.log(i)
+    i++;
+}
---
+++ js/object.js
@@ -0,0 +1,5 @@
+// for loop
+
+for(let i=1;i<=10;i++){
+    console.log("2" * i, "=",i*2);
+}
