1bc5e0db8e2a90775ed96095db60fd322dd80183
knking <knr020498@gmail.com>
2023-01-14 00:15:53 +0530

    js loop

--- js/object.js
+++ js/object.js
@@ -1,13 +1,29 @@
 
-let user={
-    name:"krishns",
-    age:25,
-    email:"knr020498@gmail.com",
-    dream:function(){
-        return "Lots of money";
-    },
-    hardworker :true,
-}
-console.table(user)
+// let user={
+//     name:"krishns",
+//     age:25,
+//     email:"knr020498@gmail.com",
+//     dream:function(){
+//         return "Lots of money";
+//     },
+//     hardworker :true,
+// }
+// console.table(user)
 
-console.log(user.dream());
+// console.log(user.dream());
+
+const obje1={
+    name:"krishna",
+    roll_num:"1111",
+    age:23,
+    set:()=>{
+        this.age=24
+    },
+
+    getAge: function(){
+        return age;
+    },
+}
+console.table(obje1)
+console.log(obje1.getAge())
+console.table(obje1)
