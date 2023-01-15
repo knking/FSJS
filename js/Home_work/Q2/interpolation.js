6ff2b97274fdcfbd08dfd7ccc7619811b92631b1
knking <knr020498@gmail.com>
2023-01-15 21:22:35 +0530

    js Q2

---
+++ js/Home_work/Q2/interpolation.js
@@ -0,0 +1,13 @@
+
+
+//02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+let firstName = "Krishnanand";
+let lastName="Roy";
+let maritalStatus = "Single";
+let age = 25;
+let country= "India";
+
+console.log("+++++++++++++++++++++++  String interpolation demo  ++++++++++++++++++++++")
+
+console.log(`My Name is ${firstName} ${lastName} age  ${age}. Marital Status  ${maritalStatus} and my country is The great ${country}`);
