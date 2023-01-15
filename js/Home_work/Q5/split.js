3f0e2561d90715556e25f54abdfec1a8e339af51
knking <knr020498@gmail.com>
2023-01-15 22:05:34 +0530

    Q5-> splited method

---
+++ js/Home_work/Q5/split.js
@@ -0,0 +1,33 @@
+
+//05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+
+
+
+let myString = " i am learing full stack we developent from pw skills";
+console.log(myString.split())
+
+let splited_string=myString.split();
+
+console.log(splited_string.length)
+
+let splied1= myString.split(" ");
+console.log(splied1)
+console.log(splied1.length)
+
+// const str = 'The quick brown fox jumps over the lazy dog.';
+
+// // const words = str.split(' ');
+// // console.log(words)
+// // console.log(words[3]);
+// // Expected output: "fox"
+
+// // const chars = str.split('');
+// // console.log(chars)
+// // console.log(chars[8]);
+// // Expected output: "k"
+
+// const strCopy = str.split();
+
+// console.log(strCopy);
+
