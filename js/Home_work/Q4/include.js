0f1a0f8e9949a57dd458af64e95f270296581c12
knking <knr020498@gmail.com>
2023-01-15 21:38:19 +0530

    Q4-> includes method

---
+++ js/Home_work/Q4/include.js
@@ -0,0 +1,13 @@
+
+
+//04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+
+let str = "can you please write a Script for me to check DB connection";
+
+console.log(str.includes("Script"));
+
+
+let str1 = "i am writing here wrong spelling of scritp";
+
+console.log(str1.includes("script"));
