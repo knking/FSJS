970d2294f5b15dacace47f0e172e5693df12e2bb
knking <knr020498@gmail.com>
2023-01-15 21:27:39 +0530

    toUpper

---
+++ js/Home_work/Q3/upperCase.js
@@ -0,0 +1,12 @@
+
+
+//03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+let myFullName = "krishnanand roy";
+
+let str = "chane me into upper case";
+
+console.log(str.toUpperCase());
+console.log(myFullName.toUpperCase())
+
+console.log(str.toUpperCase)
