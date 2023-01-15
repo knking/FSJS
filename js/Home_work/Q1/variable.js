67e8c22215b823fc407b4428cde507eb1f0e0bc3
knking <knr020498@gmail.com>
2023-01-15 16:07:05 +0530

    js home  work Q1

---
+++ js/Home_work/Q1/variable.js
@@ -0,0 +1,35 @@
+//01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+var iAmVar="knroy";
+var num=32;
+var dec=34.5;
+var i_am_undef;
+var undef=undefined;
+var nul=null;
+var bool=true
+console.log("I am Demo of var ", iAmVar,"and my type is ",typeof(iAmVar));
+console.log("I am Demo of var ", num,"and my type is ",typeof(num));
+console.log("I am Demo of var ", dec,"and my type is ",typeof(dec));
+console.log("I am Demo of var ", i_am_undef,"and my type is ",typeof(i_am_undef));
+console.log("I am Demo of var ", undef,"and my type is ",typeof(undef));
+console.log("I am Demo of var ", nul,"and my type is ",typeof(nul));
+console.log("I am Demo of var ", bool,"and my type is ",typeof(bool));
+
+let iAmLet="my name is krishna";
+let i_am_let=45;
+let decmi=12.23;
+let un=undefined;
+let boolValue=false;
+let null_type=null;
+
+const name="krishna";
+const num2=100;
+const undf = undefined;
+const bl=true;
+const n=null;
+
+
+
+
+
+
