4223bb513941d111da4f103583e810e22b8dca47
knking <knr020498@gmail.com>
2023-01-15 22:12:54 +0530

    Q6-> splited method

---
+++ js/Home_work/Q6/split2.js
@@ -0,0 +1,11 @@
+
+
+//06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+let myStr = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+
+let ansStr = myStr.split(",");
+
+console.log(ansStr)
+
+console.log("length of splited string is " , ansStr.length)
