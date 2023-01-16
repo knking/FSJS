ef3fe8f77814fe5c21957f1fb8dccd7fac12f6ce
knking <knr020498@gmail.com>
2023-01-16 07:34:58 +0530

    Q7 -> lastindexof

---
+++ js/Home_work/Q7/array.js
@@ -0,0 +1,9 @@
+
+
+//07. Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+
+let arr = ["ineuron","pw skills","google","facebook","amazone","flipkart","pw skills","payPal","zomato"]
+console.log(arr.lastIndexOf("pw skills"))
+
+
