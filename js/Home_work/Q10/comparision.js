1205e9202ee47f2a37562c4d46dd67f1b5c1ab57
knking <knr020498@gmail.com>
2023-01-16 11:59:52 +0530

    Q10 -> comparision

---
+++ js/Home_work/Q10/comparision.js
@@ -0,0 +1,55 @@
+
+
+// 10. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+// - 4 > 3
+// - 4 >= 3
+// - 4 < 3
+// - 4 <= 3
+// - 4 == 4
+// - 4 === 4
+// - 4 != 4
+// - 4 !== 4
+// - 4 != '4'
+// - 4 == '4'
+// - 4 === '4'
+// - Find the length of python and jargon and make a falsy comparison statement.
+
+
+console.log("---------below output is without console log ----------------")
+// - 4 > 3      O/P -> true
+// - 4 >= 3     O/P -> true
+// - 4 < 3      O/P -> false
+// - 4 <= 3     O/P -> false
+// - 4 == 4     O/P -> true
+// - 4 === 4    O/P -> true
+// - 4 != 4     O/P -> false
+// - 4 !== 4    O/P -> false
+// - 4 != '4'   O/P -> false
+// - 4 == '4'   O/P -> true
+// - 4 === '4'  O/P -> false
+
+console.log("4 > 3:- ",4 > 3)
+console.log("4 >= 3:- ",4 >= 3)
+console.log("4 < 3:- ",4 < 3)
+console.log("4 <= 3:- ",4 <= 3)
+console.log( " 4 == 4:- ",4 == 4 )
+console.log( " 4 === 4:- ",4 === 4 )
+console.log("4 != 4 :- ", 4 != 4)
+console.log(" 4 !== 4 :-,", 4 !== 4 )
+console.log("4 != '4' :- ", 4 != '4')
+console.log("4 == '4':- ",4 == '4')
+console.log("4 === '4' :- ", 4 === '4')
+
+console.log("------ below are consloe log output ---------")
+
+// 4 > 3:-  true
+// 4 >= 3:-  true
+// 4 < 3:-  false
+// 4 <= 3:-  false
+//  4 == 4:-  true
+//  4 === 4:-  true
+// 4 != 4 :-  false
+//  4 !== 4 :-, false
+// 4 != '4' :-  false
+// 4 == '4':-  true
+// 4 === '4' :-  false
