f909d9338b1f7ae0471e085c8e787c9851efe1a0
knking <knr020498@gmail.com>
2023-01-17 19:01:06 +0530

    Q17-> days in month

---
+++ js/Home_work/Q17/days-in-month.js
@@ -0,0 +1,14 @@
+// 17. Write a program which tells the number of days in a month.
+
+let month = "NovembeR";
+month=month.toLowerCase();
+
+if(month==="january" || month==="march" || month==="may" || month==="july" || month==="august" || month==="october" || month==="december"){
+    console.log(`Total number od days in ${month} is 31`)
+}else if(month==="april" || month==="june" || month==="sepetember" || month==="november"){
+    console.log(`Total number of days in ${month} is 30`)
+}else if(month==="february"){
+    console.log(`Total number od days in ${month} is 28 and In Leap year total number of days is 29`)
+}else{
+    console.log("Please Enter correct month name")
+}
