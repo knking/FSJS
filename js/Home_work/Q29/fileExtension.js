adfb61b33d30976a7e4c9dd09fac2afafd1242ee
knking <knr020498@gmail.com>
2023-01-25 23:40:37 +0530

    file extension

---
+++ js/Home_work/Q29/fileExtension.js
@@ -0,0 +1,9 @@
+
+// 29. Write a javascript program which takes the input of filename as string and prints the extension of the file in the console.
+
+let  fileName= "abc.jpg"
+
+let  fileExtension= fileName.split(".").pop();
+
+console.log(fileExtension)
+
