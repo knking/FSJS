41b4178323f5de872c9a5e4b520af768540a7742
knking <knr020498@gmail.com>
2023-01-25 09:26:05 +0530

    total prime

---
+++ js/Home_work/Q27/total_prime.js
@@ -0,0 +1,29 @@
+
+// 27. Write a program to print all the prime number between 0 to 100 (0 and 100 included).
+
+
+
+function checkPrime(n)
+{
+	if(n == 1 || n == 0)
+     return false;
+	
+	for(var i = 2; i < n; i++)
+	{
+		
+		if(n % i == 0)
+         return false;
+	}
+	return true;
+}
+
+
+let num = 100;
+
+for(var i = 1; i <=num; i++)
+{
+	if(checkPrime(i)) {
+		console.log(i);
+	}
+}
+
