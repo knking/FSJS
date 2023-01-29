bf563249d00cb2512600184e15b1969dd07621db
knking <knr020498@gmail.com>
2023-01-29 08:00:48 +0530

    calculator

--- js/Home_work/Q30/calculator.js
+++ js/Home_work/Q30/calculator.js
@@ -2,16 +2,38 @@
 // 30. Create a simple calculator program in JavaScript which can perform the addition, substraction, multiplication and division on given numbers.
 
 
-function calculator(num1,num2,operation){
-    if(operation =="+"){
-        return num1+num2;
-    }if(operation==="-"){
-        return num1-num2;
-    }if(operation==="*"){
-        return num1 * num2;
-    }if(operation==="%" || operation ==="/"){
-        return num1 / num2;
-    }
+
+let num1 =10;
+let num2 =20;
+
+let operation = "substractio";
+
+switch (operation) {
+    case "add":
+        console.log(num1+num2)
+        break;
+    case "substraction":
+        console.log(num1-num2)
+        break;
+    case "multipilcation":
+        console.log(num1*num2)
+    case "division":
+        console.log(num1/num2)
+    default:
+        console.log("Pleae enter correct operatot")
+        break;
 }
 
-console.log(calculator(10,20,"*"))
+// function calculator(num1,num2,operation){
+//     if(operation =="+"){
+//         return num1+num2;
+//     }if(operation==="-"){
+//         return num1-num2;
+//     }if(operation==="*"){
+//         return num1 * num2;
+//     }if(operation==="%" || operation ==="/"){
+//         return num1 / num2;
+//     }
+// }
+
+// console.log(calculator(10,20,"*"))
