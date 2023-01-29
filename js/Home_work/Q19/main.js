a9a4417ec6f6d73b07d536d7b64bb330897f72d8
knking <knr020498@gmail.com>
2023-01-29 09:56:51 +0530

    import export

--- js/Home_work/Q19/main.js
+++ js/Home_work/Q19/main.js
@@ -1,8 +1,10 @@
 
 //here trying to access both countries and web techs files
 
-import {
-     coun
-     } from "./countries.js";
+import { coun } from "./countries.js";
+
+import { webTechs } from "./web_techs.js";
 
 console.log(coun)
+console.log(webTechs)
+
--- js/Home_work/Q19/web_techs.js
+++ js/Home_work/Q19/web_techs.js
@@ -2,3 +2,6 @@
 //web techs
 
 let web_techs = ["React","Angular js","Node js","Ruby on Rails","Laravel","Ddjango","Express js","Spring"]
+
+
+export let webTechs=web_techs;
