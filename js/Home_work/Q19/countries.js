f87ee96844e562ae0141d7609da940f120d03755
knking <knr020498@gmail.com>
2023-01-29 09:55:12 +0530

    import export

--- js/Home_work/Q19/countries.js
+++ js/Home_work/Q19/countries.js
@@ -2,4 +2,7 @@
 
 //countries name
 
-let countries = ["India","Nepal","Sri lanka","Japan","England","America"]
+ let countries = ["India","Nepal","Sri lanka","Japan","England","America"]
+
+export let coun=countries;
+
---
+++ js/Home_work/Q19/main.js
@@ -0,0 +1,8 @@
+
+//here trying to access both countries and web techs files
+
+import {
+     coun
+     } from "./countries.js";
+
+console.log(coun)
