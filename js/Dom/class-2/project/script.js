c5a68610b38859685c223042fb9c9ff0e60b0d61
knking <knr020498@gmail.com>
2023-02-01 09:41:55 +0530

    color changer project

---
+++ js/Dom/class-2/project/script.js
@@ -0,0 +1,22 @@
+
+let btn = document.getElementById("button");
+
+
+const randomColor= ()=>{
+    let val = "0123456789ABCDEF";
+
+    let color = "#";
+
+    for(let i=0;i<6;i++){
+        color = color+ val[Math.floor(Math.random() * 16)];
+
+    }
+
+    return color;
+}
+
+function changeColor(){
+    document.body.style.backgroundColor=randomColor()
+}
+btn.addEventListener("click",changeColor)
+    
