6337a19a5ba7d346541cbe3b01c79db2ccdb82d8
knking <knr020498@gmail.com>
2023-02-04 19:32:18 +0530

    Dom project 2

--- js/Dom/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
+++ js/Dom/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
@@ -1,27 +1,4 @@
 
-//one
-// let para = document.querySelectorAll(".accordian-wrapper p");
-// para[2].style.display="block";
-// para[3].style.display="block";
-
-//two
-
-let elem = document.createElement("div");
-elem.className="accordian";
-document.querySelector(".accordian-wrapper").appendChild(elem)
-// elem.setAttribute('class','accordian')
-
-let head3= document.createElement("h3");
-let newPara = document.createElement("p");
-
-
-head3.innerText="Skills";
-newPara.innerText="i Posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the Github.";
-
-elem.appendChild(head3)
-elem.append(newPara)
-
-
 let accordian = document.querySelectorAll(".accordian h3");
 accordian.forEach((element) => {
   element.addEventListener("click", () => {
@@ -34,6 +11,44 @@ accordian.forEach((element) => {
     }
   });
 });
+
+                            //mycode
+
+//one
+// let para = document.querySelectorAll(".accordian-wrapper p");
+// para[2].style.display="block";
+// para[3].style.display="block";
+
+//two
+
+// let elem = document.createElement("div");
+// elem.className="accordian";
+// document.querySelector(".accordian-wrapper").appendChild(elem)
+// elem.setAttribute('class','accordian')
+
+// let head3= document.createElement("h3");
+// let newPara = document.createElement("p");
+
+
+// head3.innerText="Skills";
+// newPara.innerText="i Posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the Github.";
+
+// elem.appendChild(head3)
+// elem.append(newPara)
+
+
+// let accordian = document.querySelectorAll(".accordian h3");
+// accordian.forEach((element) => {
+//   element.addEventListener("click", () => {
+//     let para = element.nextElementSibling;
+//     console.log(para)
+//     if (para.style.display === "block") {
+//       para.style.display = "none";
+//     } else {
+//       para.style.display = "block";
+//     }
+//   });
+// });
 
 
 // Array.from(accordian);
---
+++ js/Dom/demo/script.js
@@ -0,0 +1,11 @@
+
+
+const checkbox = document.querySelector("#id-checkbox");
+
+checkbox.addEventListener("click", checkboxClick, false);
+
+function checkboxClick(event) {
+  let warn = "preventDefault() won't let you check this!<br>";
+  document.getElementById("output-box").innerHTML += warn;
+//   event.preventDefault();
+}
