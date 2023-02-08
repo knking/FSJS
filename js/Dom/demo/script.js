cb9c731f232965f6362a7f7665b1d6c699f4c395
knking <knr020498@gmail.com>
2023-02-08 22:42:14 +0530

    Dom project

--- js/Dom/demo/script.js
+++ js/Dom/demo/script.js
@@ -1,11 +1,18 @@
 
-
-const checkbox = document.querySelector("#id-checkbox");
-
-checkbox.addEventListener("click", checkboxClick, false);
-
-function checkboxClick(event) {
-  let warn = "preventDefault() won't let you check this!<br>";
-  document.getElementById("output-box").innerHTML += warn;
-//   event.preventDefault();
-}
+let button = document.querySelector('#button');
+button.addEventListener('mouseup', (e) => {
+  let log = document.querySelector('#log');
+  switch (e.button) {
+    case 0:
+      log.textContent = 'Left button clicked.';
+      break;
+    case 1:
+      log.textContent = 'Middle button clicked.';
+      break;
+    case 2:
+      log.textContent = 'Right button clicked.';
+      break;
+    default:
+      log.textContent = `Unknown button code: ${e.button}`;
+  }
+});
