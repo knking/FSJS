2abfcde88f5eceff0ad1323c55ee3dfbb252b5ec
knking <knr020498@gmail.com>
2023-02-11 21:50:01 +0530

    Dom Assignment 7 part Two

--- js/Dom/demo/script.js
+++ js/Dom/demo/script.js
@@ -1,18 +1,22 @@
 
-let button = document.querySelector('#button');
-button.addEventListener('mouseup', (e) => {
-  let log = document.querySelector('#log');
-  switch (e.button) {
-    case 0:
-      log.textContent = 'Left button clicked.';
-      break;
-    case 1:
-      log.textContent = 'Middle button clicked.';
-      break;
-    case 2:
-      log.textContent = 'Right button clicked.';
-      break;
-    default:
-      log.textContent = `Unknown button code: ${e.button}`;
-  }
-});
+// let button = document.querySelector('#button');
+// button.addEventListener('mouseup', (e) => {
+//   let log = document.querySelector('#log');
+//   switch (e.button) {
+//     case 0:
+//       log.textContent = 'Left button clicked.';
+//       break;
+//     case 1:
+//       log.textContent = 'Middle button clicked.';
+//       break;
+//     case 2:
+//       log.textContent = 'Right button clicked.';
+//       break;
+//     default:
+//       log.textContent = `Unknown button code: ${e.button}`;
+//   }
+// });
+
+// setTimeout(() => {
+//   document.location.reload();
+// }, 3000);
