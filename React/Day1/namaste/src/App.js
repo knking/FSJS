5de70de5179e9c3b2eef7de196e56e1f24a35521
knking <knr020498@gmail.com>
2023-02-20 20:40:19 +0530

    react day2

--- React/Day1/namaste/src/App.js
+++ React/Day1/namaste/src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+function Counter(){
+  let [count, setCount] = useState(100)
+
+  function oneUp(){
+     setCount(count + 1)
+  }
+
+  return(
+    <div>
+      <button
+      onClick={oneUp}
+      >Count : {count}</button>
+    
     </div>
-  );
+  )
 }
+function App(){
+  return (
+    <>
+    <h1>I am Learning React </h1>
+    <button>Click me bro</button>
+    <Counter/>
+    </>
+  )
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
