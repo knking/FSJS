170169a10cc7eadda57690e078d484b4d503db06
knking <knr020498@gmail.com>
2023-02-23 20:44:32 +0530

    react custom compomnent

---
+++ React/Day3/reusable-component/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css'
+
+const App = () => {
+  return (
+
+    <div>
+      <h1>Hellow from react</h1>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App
