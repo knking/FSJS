67c408fa8c63ad76c013730b51052dbaba022bd3
knking <knr020498@gmail.com>
2023-03-03 19:47:09 +0530

    tictactoe updated

---
+++ React/self/web-two/src/App.js
@@ -0,0 +1,26 @@
+
+import React from 'react'
+import NavBar from './Navbar'
+import HeroSection from './HeroSection';
+import AppSection from './AppSection';
+import CardSection from './CardSection';
+import Footer from './Footer'
+
+const App = () => {
+    return (
+        <div>
+        <NavBar/>
+        <HeroSection/>
+        <CardSection/>
+        <AppSection/>
+        
+        <Footer/>
+
+        </div>
+    
+    );
+        
+};
+
+
+export default App
