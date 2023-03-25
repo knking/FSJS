9dd08c455c7393abfe3ed9b625db1073e5777a96
knking <knr020498@gmail.com>
2023-03-26 00:00:49 +0530

    react input field

---
+++ React/practice/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
+  return (
+    <div style={{height:'700px',width:'500px', display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+      <form>
+      <input
+        type="text"
+        id="message"
+        name="message"
+        placeholder='Enter Color Name'
+        onChange={handleChange}
+        value={message}
+      />
+      </form>
+      <h2>Message: {message}</h2>
+
+      <div style={{height:'50px',width:'80px',border:'1px solid red',backgroundColor:`${message}`}}>
+
+</div>
+    </div>
+  );
+};
+
+export default App;
+
