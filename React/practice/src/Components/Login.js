d202d74a643bc9a8aed2c17fb0acaf22934d3eaa
knking <knr020498@gmail.com>
2023-03-27 17:24:56 +0530

    react login form

--- React/practice/src/Components/Login.js
+++ React/practice/src/Components/Login.js
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  "./Style.css"
 
 function Login() {
 
+  const [firstName, setFirstName]= useState("")
+  const [lastName,setLastName]= useState("")
+  const  [fullNmae, setFullName]= useState("")
+
     const ClickMe = (e)=>{
-        console.log("I am clicked")
+         setFirstName(e.target.value)
     }
+    const ClickMe2 = (e)=>{
+          setLastName(e.target.value)
+    }
+    const sendData= (event)=>{
+      event.preventDefault()
+   setFullName(firstName+" "+lastName)
+    }
+   
   return (
     <div>
-        <h1>Hello</h1>
+        <h1>Hello, {fullNmae}</h1> 
       <form>
 
         <input
         type="text"
         placeholder='Enter your Fisrt name'
         onChange={ClickMe}
+        value={firstName}
         />
         <br/>
 
        <input
         type="text"
         placeholder='Enter your Last Name '
-        onChange={ClickMe}
+        onChange={ClickMe2}
+        value={lastName}
         />
         <br/>
-        <button type='submit'>Submit</button>
+        <button onClick={sendData}>Submit</button>
       </form>
 
     </div>
