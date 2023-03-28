e53e5882b0b3824ba1e057fa3ff4d0e121303669
knking <knr020498@gmail.com>
2023-03-28 11:53:15 +0530

    React Login form

--- React/practice/src/Components/Login.js
+++ React/practice/src/Components/Login.js
@@ -3,39 +3,75 @@ import  "./Style.css"
 
 function Login() {
 
-  const [firstName, setFirstName]= useState("")
-  const [lastName,setLastName]= useState("")
-  const  [fullNmae, setFullName]= useState("")
+  // const [firstName, setFirstName]= useState("")
+  // const [lastName,setLastName]= useState("")
+  // const  [fullNmae, setFullName]= useState("")
 
-    const ClickMe = (e)=>{
-         setFirstName(e.target.value)
-    }
-    const ClickMe2 = (e)=>{
-          setLastName(e.target.value)
-    }
+  const [values,setValues]= useState({
+    firstName:"",
+    lastName:"",
+    number:"",
+  });
+
+    const ClickMe = (event)=>{
+        //  setFirstName(e.target.value)
+        // setValues({...values,firstName:e.target.value,lastName:e.target.value})
+
+        const {name,value} = event.target;
+    
+    // const ClickMe2 = (e)=>{
+    //       // setLastName(e.target.value)
+    //       setValues({...values,lastName:e.target.value})
+    // }
+
+    setValues((preValue)=>{
+      return{
+       ...preValue,
+      [name]: value
+
+      }   
+    })
+  }
     const sendData= (event)=>{
-      event.preventDefault()
-   setFullName(firstName+" "+lastName)
+       event.preventDefault()
+  //  setFullName(firstName+" "+lastName)
+  console.log(values)
     }
    
   return (
     <div>
-        <h1>Hello, {fullNmae}</h1> 
+        <h1>Hello</h1> 
+        <p>{values.firstName}</p>
+        <p>{values.lastName}</p>
+        <p>{values.number}</p>
       <form>
 
         <input
         type="text"
         placeholder='Enter your Fisrt name'
         onChange={ClickMe}
-        value={firstName}
+        // value={firstName}
+        name="firstName"
+        value={values.firstName}
         />
         <br/>
 
        <input
         type="text"
         placeholder='Enter your Last Name '
-        onChange={ClickMe2}
-        value={lastName}
+        onChange={ClickMe}
+        // value={lastName}
+        name="lastName"
+        value={values.lastName}
+        />
+        <br/>
+        <input
+        type="nmber"
+        placeholder='Enter your mobile '
+        onChange={ClickMe}
+        // value={lastName}
+        name="number"
+        value={values.number}
         />
         <br/>
         <button onClick={sendData}>Submit</button>
