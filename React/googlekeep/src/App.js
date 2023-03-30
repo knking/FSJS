e1dbc814062a20db85cbe6bc8be0dc89e717f823
knking <knr020498@gmail.com>
2023-03-30 17:38:19 +0530

    google keep app in react

---
+++ React/googlekeep/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/googlekeep/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>I AM Header</h1>
+    </div>
+  )
+}
+
+export default Header
