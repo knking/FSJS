e04a8bac0614678d439605cba524e1a7ca96d661
knking <knr020498@gmail.com>
2023-04-09 09:09:47 +0530

    react chakra ui

--- React/googlekeep/src/Components/CreateNote.js
+++ React/googlekeep/src/Components/CreateNote.js
@@ -35,10 +35,7 @@ const CreateNote = (props) => {
      title:"",
     content:""
     })
-    
-    
   };
-
 
   return (
     <>
---
+++ React/youtube/project2/src/App.js
@@ -0,0 +1,16 @@
+import { Button } from '@chakra-ui/react';
+import React from 'react';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Header from './components/Header';
+function App() {
+  return (
+   <Router>
+    <Header/>
+    <Routes>
+      <Route/>
+    </Routes>
+   </Router>
+  );
+}
+
+export default App;
