23fe9e8af930805b91de222f8bc37b48d826d9c4
knking <knr020498@gmail.com>
2023-05-26 19:03:34 +0530

    first express app

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -5,12 +5,12 @@ const app = express()
 const PORT =3000;
 
 app.get("/",(req,res)=>{
-res.send("hellow world")
+res.status(200).send("hellow world")
 })
 
 
 app.get("/name",(req,res)=>{
-    res.send("My Name is Krishna")
+    res.status(201).send("My Name is Krishna")
 })
 app.listen(PORT,()=>{
     console.log("Server is Running...")
